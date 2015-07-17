e2604b719fffc2dd31ef0ffcf8a1ade6e879b12b
Arco Mul <me@arcomul.nl>
2015-07-17 22:31:53 +0200

    Fix local path

--- wp-content/db.php
+++ wp-content/db.php
@@ -3,8 +3,8 @@
 add_filter ( 'pre_option_home', 'test_localhosts' );
 add_filter ( 'pre_option_siteurl', 'test_localhosts' );
 function test_localhosts( ) {
-    if (false) {
-        return "htp://127.0.0.1:8080";
+    if (true) {
+        return "http://127.0.0.1:8080";
     }
     return false;
 }
